80bab30e0e151b2d7b9a8a1dc4db249f603c9e49
KaiserLycan <josephreyramirezpanti+github@gmail.com>
2025-02-22 12:00:26 +0800

    Final Version of FileManager.Java

--- src/MyLib/FileManager.java
+++ src/MyLib/FileManager.java
@@ -14,141 +14,248 @@ import java.util.Scanner;
  *
  * @author Joseph Rey
  */
+
+/**
+ * This class handles access or any modifications done to a text files stored in the TextFiles folder.
+ * 
+ */
 public class FileManager {
-    private File file = null;
-    private FileWriter writer = null;
-    private Scanner scan = null;
-    private boolean ignoreHeader;
+    //This value is not allowed to change in any part 
+    //of the program aside from the instantiation.
+    private final File file;    
+    //This is used to ignore the header of the 
+    //table stored in the textfile.
+    private boolean ignoreHeader; 
     
-    private char getChoice(String prompt) {
-        char choice;
-        scan = new Scanner(System.in);
-        do {
-            System.out.print(prompt);
-            choice = scan.next().toLowerCase().charAt(0);
-            
-            if(choice != 'y' && choice != 'n') {
-                System.out.println("Invalid Input. Try again...");
-            }
-        } while(choice != 'y' && choice != 'n');
-        
-        return choice;
+    ////////////////////////////////////////////////////////////////////////////
+    //Constructor
+    /**
+     * @param file Requires an existing file.
+     */
+    public FileManager(File file) {
+        this.file = file;
     }
     
-    public boolean openFile(String filename) {
-        scan = new Scanner(System.in);
-        char choice; 
-        file = new File("./src/TextFiles/" + filename + ".txt");
-        
-        if(!file.exists()) {
-            choice = getChoice("Do you want to create " + filename + ".txt?[y/n]: "); 
-            if(choice == 'y') {
-                this.createFile(file);
-            }
-            else {
-                return false;
-            }
-        }
-        
-        return true;   
-    }
+    ////////////////////////////////////////////////////////////////////////////
+    // User-Define Methods
     
-    public void createFile(File file) {
-        try{
-            file.createNewFile();
-            System.out.println( file.getName() + " has been created.");
-        }
-        catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        } 
-    }
-    
-    public void deleteFile() {
-        if (file.delete()) {
-            System.out.println(file.getName() + " has been deleted.");
-        }
-        else {
-            System.out.println("Cannot delete file");
-        }
-    }
-    
-    public ArrayList<String[]> extractData(Boolean ignoreHeader) {
-        ArrayList<String[]> table = new ArrayList<String[]>();
+    /**
+     * Extracts/Access the data stored in the text file.
+     * @param ignoreHeader  If TRUE extract file content. If FALSE extract file content and header.
+     * @return ArrayList<String[]>  List of rows containing columns of data extracted from the file
+     */
+    public ArrayList<String[]> extractData(boolean ignoreHeader) {
+        ArrayList<String[]> table = new ArrayList<>(); //List to return
         this.ignoreHeader = ignoreHeader;
         
-        
-        try {
-            scan = new Scanner(file);
-        }
-        catch(IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        
-        while(scan.hasNextLine()) {
+        try (Scanner scan = new Scanner(file)) {
+            while(scan.hasNextLine()) {
             if(this.ignoreHeader) {
                 this.ignoreHeader = false;
                 scan.nextLine();
                 continue;
             }
-            
-            table.add(scan.nextLine().split("\\t*\\s"));
+            table.add(scan.nextLine().split("\\t+\\s"));
+            }
+        }
+        catch(IOException ex) {
+            System.out.println(ex.getMessage());
         }
         
         return table;
     }
     
-    public void addData(Object object) {
-        try {
-            writer = new FileWriter(file, true);
-            writer.append(object.toString());
-            writer.close();
-        }
-        catch(IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+    /**
+     * Checks if the data exists in the file.
+     * If the textFile consist of only header the method will automatically
+     * return FALSE.
+     * @param data An instance of a class that implements IGenerateID inteface.
+     * @return Boolean  TRUE if the ID of the data exist or a copy of the conent
+     *                  of the data exist in the text file. FALSE if both are
+     *                  not true.
+     */
     
-    public void removeData(IGenerateID object) {
-        ignoreHeader = true;
-        ArrayList<String[]> table = this.extractData(false);
-        if(this.dataExists(object)) {
-            for(int i = 1; i > table.size(); i++) {
-                if(Long.parseLong(table.get(i)[0]) == object.getID()) {
-                    table.remove(i);
-                    break;
-                }
-            }
-            
-            try {
-                writer = new FileWriter(file);
-                for(String[] row : table) {
-                    String.join("\t\t\t", row);
-
-                }
-            }
-            catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
-    
-    public boolean dataExists(IGenerateID object) {
-        boolean exists = false;
-        long objectID = object.getID();
+    public boolean dataExists(IGenerateID data) {
         ArrayList<String[]> table = this.extractData(true);
+        boolean exists = false;
+        long contentID;
+        String[] content;
+        
+        if(table.isEmpty()) {
+            return false;
+        }
+
+        content = new String[table.get(0).length - 1];
         
         for(String[] row : table) {
-            System.out.println(row[0]);
-            System.out.println(objectID);
-            if(Long.parseLong(row[0]) == objectID) {
+            
+            //Convert the content ID from String to Long
+            try{
+                contentID = Long.parseLong(row[0]);
+            }
+            catch(NumberFormatException ex) {
+                System.out.println(ex.getMessage());
+                break;
+            }
+            
+            //Checkes if user exist based on ID if not then check if the content
+            //has existing copies.
+            if(contentID == data.getID()) {
                 exists = true;
                 break;
+            }
+            else {
+                /*
+                How does this work?
+                Ignoring the id, this program will make a copy of the conent
+                of the current row. Which later will be turned into a string
+                and compared to the cotnent of the data parameter.
+                
+                Essentially the code below is the better implementation of this:
+                for(int i = 1; i < row.length; i++) {
+                    dataConent = dataConent + row[i];
+                    if(!(i == row.length-1)) {
+                     dataConent = dataConent + "\t\t\t";
+                    }
+                }
+                if(content.equals(data.toString())) {
+                    exists = true;
+                    break;
+                }
+                */
+                System.arraycopy(row, 1, content, 0, row.length-1);
+                if(data.toString().equals(String.join("\t\t\t", content))) {
+                    exists = true;
+                    break;
+                }
+                
             }
         }
         
         return exists;
     }
     
+    /**
+     * Appends data onto the file but only if the data doesn't already exist.
+     * @param  data an instance of a class that implements IGenerateID interface
+     */
+    public void addData(IGenerateID data) {
+        if(!this.dataExists(data)) {
+            /*
+            How does this work?
+            Objects like FileWriter and Scanner needs to be closed to avoid
+            data leakage and sometimes require exception handling. 
+            This syntax. also known as try-with-resources, automatically closes
+            the resources (e.g. FileWriter), while also handling exceptions.
+            */
+            try (FileWriter writer = new FileWriter(file, true)) {
+                writer.append( data.getID() + "\t\t\t"+ data.toString() + '\n');
+            }
+            catch(IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            System.out.println("Data has been added");
+        }
+        else {
+            System.out.println("Cannot add existing data.");
+        }
+    }
     
+    /**
+     * Changes the values of data in the file but only if it exist.
+     * @param data an instance of a class that implements IGenerateID interface.
+     */
+    public void updateData(IGenerateID data) {
+        ArrayList<String[]> table = this.extractData(false);
+        String[] content = data.toString().split("\\t+\\s");
+        long contentID = 0l;
+        
+        if(this.dataExists(data)) {
+            ignoreHeader = true;
+            for(String[] row : table) {
+                if(ignoreHeader) {
+                    ignoreHeader = false;
+                    continue;
+                }
+                
+                try {
+                    contentID = Long.parseLong(row[0]);
+                }
+                catch(NumberFormatException ex) {
+                    ex.printStackTrace();
+                }
+                
+                if(contentID == data.getID()) {
+                    /*
+                    This code below essentially copies the components of from 
+                    conent array starting at it's first to last componets 
+                    onto the array[] that matches the ID of the data.
+                    
+                    Essentially it is the better implmentation of this code:
+                    for(int j = 0; j < content.size; j++) {
+                        row[j+1] = content[0];
+                    }
+                    */
+                    System.arraycopy(content, 0, row, 1, content.length);
+                }
+            }
+            
+            // Re-write the entire file but with the udpated data.
+            try (FileWriter writer = new FileWriter(file)) {
+                for(String[] row: table) {
+                    writer.append(String.join("\t\t\t", row) + '\n');
+                }
+            }
+            catch(IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            System.out.println("File Updated");
+            
+        }
+        else {
+            System.out.println("Cannot udpate non-existing data.");
+        }
+    }
     
-}
+    /**
+     * Removes data from the file but only if it has not have already been removed.
+     * @param data an instance of a class that implements IGeneratedID
+     */
+    public void removeData(IGenerateID data) {
+        ArrayList<String[]> table = this.extractData(false);
+        long contentID = 0l;
+        ignoreHeader = true;
+        if(this.dataExists(data)) {
+            //Remove the data from the extracted data.
+            for(int i = 1; i < table.size(); i++) {
+                try{
+                    contentID = Long.parseLong(table.get(i)[0]);
+                }
+                catch(NumberFormatException ex) {
+                    System.out.println(ex.getMessage());
+                }
+                
+                if(contentID == data.getID()) {
+                    table.remove(i);
+                    break;
+                }
+            }
+            
+            
+            //Re-write the file but without the removed data.
+            try (FileWriter writer = new FileWriter(file)) {
+                for(String[] row : table) {
+                    writer.append(String.join("\t\t\t", row) + '\n');
+                }
+            }
+            catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            
+            System.out.println("Data has been removed");
+        }
+        else {
+            System.out.println("Data has already been removed.");
+        }
+    }
+}// end of FileManager Class
